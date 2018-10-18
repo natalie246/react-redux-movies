70a04251ede05c336f3150d627ae5d97b4302f88
liron <liron@ensilo.com>
2018-10-18 10:37:27 +0300

    Updates

---
+++ precache-manifest.e655851a78ce6dc88273d59c312c2b97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2bd5b30e158582d5a090",
+    "url": "/react-redux-movies/static/js/runtime~main.2bd5b30e.js"
+  },
+  {
+    "revision": "02a8fc48428211ad7952",
+    "url": "/react-redux-movies/static/js/main.02a8fc48.chunk.js"
+  },
+  {
+    "revision": "3ab354a5d3375a3d9991",
+    "url": "/react-redux-movies/static/js/1.3ab354a5.chunk.js"
+  },
+  {
+    "revision": "02a8fc48428211ad7952",
+    "url": "/react-redux-movies/static/css/main.ba03f5e2.chunk.css"
+  },
+  {
+    "revision": "7f1d8b35332ba87306b62fa46b9a607d",
+    "url": "/react-redux-movies/index.html"
+  }
+];
